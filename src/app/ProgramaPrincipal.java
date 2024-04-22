9fe6f99db52e0fa3b47178db28cd7b42da44b939
Kadzerski <annagiulliask.11@gmail.com>
2024-04-22 20:51:30 -0300

    Atributos privados

--- src/app/ProgramaPrincipal.java
+++ src/app/ProgramaPrincipal.java
@@ -9,14 +9,15 @@ public class ProgramaPrincipal
 {
     public static void main(String[] args) {
         Pessoa fulano = new Pessoa();
-        fulano.nome = "Fulano da Silva";
+        fulano.setNome("Fulano da Silva");
         fulano.endereco = "Rua dos programadores, 42";
-        fulano.nascimento = LocalDate.of(1990, 10, 13);
-        fulano.salario = new BigDecimal("1000.99");
-        System.out.println("Nome......: " + fulano.nome);
+        fulano.setNascimento(LocalDate.of(1990, 10, 13));
+        fulano.setSalario(new BigDecimal("1000.99"));
+
+        System.out.println("Nome......: " + fulano.getNome());
         System.out.println("Endereço..: " + fulano.endereco);
-        System.out.println("Nascimento: " + fulano.nascimento);
-        System.out.println("Salário...: " + fulano.salario);
+        System.out.println("Nascimento: " + fulano.getNascimento());
+        System.out.println("Salário...: " + fulano.getSalario());
 
         // A linha de baixo acusa erro pois não consegue acessar o atributo nome
         // System.out.println(fulano.nome);
